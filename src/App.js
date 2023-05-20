fd7722a56fd5937db8204e8e45b769273357e941
Sarita Kharabe <98185682+saritakharabe@users.noreply.github.com>
2023-05-19 23:37:46 -0400

    created initial folders and files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import About from './component/About'
+import Contact from './component/Contact';
+import Navbar from './component/Navbar';
+import Projects from './component/Projects';
+import Skills from './component/Skills';
+import Testimonials from './component/Testimonials';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
     </div>
   );
 }
